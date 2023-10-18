b65b7ff722f324e32e65ef97554d9d8035f5452b
NejahA <achref.nejah@gmail.com>
2023-10-18 14:16:24 +0100

    day 1 and 2 commit

--- Programming Basics/Week 1/Day 3/Practice/Nejah_Achref__T-Diagram.js
+++ Programming Basics/Week 1/Day 3/Practice/Nejah_Achref__T-Diagram.js
@@ -1,21 +1,43 @@
-// Code Snippet 1
-hello,Dojo
+// // Code Snippet 1
+// hello,Dojo
 
 
-// Code Snippet 2
-result = 15
-console : result is 15
+// // Code Snippet 2
+// result = 15
+// console : result is 15
 
-// Code Snippet 3
+// // Code Snippet 3
 
-result = 18
-coneole : result is 18
+// result = 18
+// coneole : result is 18
 
-// Code Snippet 4
-var num = 15
-var result =10
-console : 10,15
+// // Code Snippet 4
+// var num = 15
+// var result =10
+// console : 10,15
 
-// Code Snippet 5
-var num = 15
-var 20
+// // Code Snippet 5
+// var num = 15
+// var result = 20
+// console : 15,20,15
+
+// // Code Snippet 6
+
+// var result = 16
+// console : result is 16
+
+// // Code Snippet 7
+// console : 5,8
+
+// // Code Snippet 8
+// console 5,8
+
+// // Code Snippet 9
+// var result = 13
+// console : result is 13 
+
+// // Code Snippet 9
+// var result = 19
+// console : result iis 19
+
+
