a6dee199b89ebc149249be2e9faf2de8f6729b82
NejahA <achref.nejah@gmail.com>
2023-10-22 18:56:41 +0100

    Problem Solving

---
+++ Programming Basics/Week 1/Day 5/Optional/Achref_Nejah_Problem_Solving.js
@@ -0,0 +1,10 @@
+// We need to scan the for pronouns in every sentence of the paragraph.
+
+// 1) create a function that has the paragraph as a parameter.
+// 2)the function divides the paragraph into sentences
+// 3)the function searches for pronouns in the sentences by comparing
+// every word to a list of stringss built in the functions that represents standard pronous,
+// 4)returs a string that indicates the perspective for each sentence
+
+
+// The hardest part is translating the developer's solution into algorithm 
