34d5dfa0c64d7f14de5819a197c74336b60415f5
NejahA <achref.nejah@gmail.com>
2023-11-14 22:20:28 +0000

    Basic For Loop Python

--- Python/Week 1/Day 1/Core/For Loops Basic I.py
+++ Python/Week 1/Day 1/Core/For Loops Basic I.py
@@ -1,27 +1,27 @@
-# for i in range(151):
-#     print(i)
+for i in range(151):
+    print(i)
 
-# for i in range(5,500):
-#     if i%5==0 :
-#         print(i)
+for i in range(5,500):
+    if i%5==0 :
+        print(i)
 
-# for i in range(1,100):
-#     if i%10==0:
-#         print("Coding Dojo")
-#     elif i%5==0:
-#         print("Coding")
-#     else:
-#         print(i)
+for i in range(1,100):
+    if i%10==0:
+        print("Coding Dojo")
+    elif i%5==0:
+        print("Coding")
+    else:
+        print(i)
 
-# sum = 0
-# for i in range(500000):
-#     if(i%2==1):
-#         sum +=i
+sum = 0
+for i in range(500000):
+    if(i%2==1):
+        sum +=i
     
-# print(sum)
+print(sum)
 
-# for i in range(2018,-1,-4):
-#     print(i)
+for i in range(2018,-1,-4):
+    print(i)
 
 lowNum=2
 highNum=9
