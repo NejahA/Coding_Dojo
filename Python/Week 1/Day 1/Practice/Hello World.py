4d9eecc0c8d658b399a9653fb55e45b6083d41f4
NejahA <achref.nejah@gmail.com>
2023-11-14 20:03:56 +0000

    Hello World

---
+++ Python/Week 1/Day 1/Practice/Hello World.py
@@ -0,0 +1,22 @@
+print("Hello World")
+
+my_name="Achref"
+
+print("Hello",my_name)
+
+print("Hello "+ my_name)
+
+fav_num= 1
+
+print("Hello", fav_num)
+
+# print("Hello"+fav_num)
+
+print("Hello "+str(fav_num))
+
+food1= "kafteji"
+food2= "shawarma"
+
+print("I love to eat {} and {}".format (food1,food2))
+
+print(f"I love to eat {food1} and {food2}")
