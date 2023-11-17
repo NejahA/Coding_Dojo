11c14e4d6fd1d83fde92363818ff50cbf313755f
NejahA <achref.nejah@gmail.com>
2023-11-17 15:05:40 +0000

    BankAccount

--- Python/Week 1/Day 2/Core/BankAccountr.py
+++ Python/Week 1/Day 2/Core/BankAccountr.py
@@ -8,21 +8,22 @@ class BankAccount:
         
     def deposit(self, amount):
         self.balance += amount
-        return self.balance
+        return self
     def withdraw(self, amount):
         if self.balance >= amount:
             self.balance-= amount
-            return self.balance
+            return self
         else:
             print("Insufficient funds: Charging a $5 fee")
             self.balance -= 5
-            return self.balance
+            return self
     def display_account_info(self):
         print(f"Balance: {self.balance}$")
+        return self
     def yield_interest(self):
         if self.balance>0:
             self.balance= self.balance*(1+ self.rate)
-            return self.balance
+            return self
     @classmethod
     def all_infos(cls):
         for i in range (0, len(cls.all_accounts)):
@@ -31,19 +32,9 @@ class BankAccount:
 acc1 = BankAccount (0.02,1000)
 acc2= BankAccount (0.05, 800)
 
-acc1.deposit(100)
-acc1.deposit(150)
-acc1.deposit(50)
-acc1.withdraw(200)
-acc1.yield_interest()
-acc1.display_account_info()
+acc1.display_account_info().deposit(100).deposit(150).deposit(50).withdraw(200).display_account_info()
 
-acc2.withdraw(100)
-acc2.withdraw(50)
-acc2.withdraw(70)
-acc2.withdraw(50)
-acc2.yield_interest()
-acc2.display_account_info()
 
-BankAccount.all_infos()
+
+
 
