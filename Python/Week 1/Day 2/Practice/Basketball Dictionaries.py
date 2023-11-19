0d3087d6bdbafe90f6836908461222ec97661d55
NejahA <achref.nejah@gmail.com>
2023-11-19 18:08:12 +0000

    Basketball Dictioonnaries

---
+++ Python/Week 1/Day 2/Practice/Basketball Dictionaries.py
@@ -0,0 +1,87 @@
+players = [
+    {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+    },
+    {
+    	"name": "Kyrie Irving", 
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Damian Lillard", 
+    	"age":33,
+        "position": "Point Guard", 
+    	"team": "Portland Trailblazers"
+    },
+    {
+    	"name": "Joel Embiid", 
+    	"age":32,
+        "position": "Power Foward", 
+    	"team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+class Player:
+    def __init__(self, player):
+        self.name = player["name"]
+        self.age = player["age"]
+        self.position = player["position"]
+        self.team = player["team"]
+
+    def stats(self):
+        print(self.name,self.age,self.position,self.team)
+
+    @classmethod
+    def get_team(cls,team_list):
+        player_list=[]
+        for i in range(0,len(team_list)) :
+            player_list.append(Player(team_list[i]))
+        return player_list
+
+
+kevin = {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+}
+jason = {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+}
+kyrie = {
+    	"name": "Kyrie Irving", 
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+}
+    
+# Create your Player instances here!
+player_kevin= Player(kevin)
+player_jason = Player(jason)
+player_kyrie= Player(kyrie)
+
+player_kevin.stats()
+
+playerClassInstacesList= Player.get_team(players)
+
+
+
