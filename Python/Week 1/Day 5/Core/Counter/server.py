36a195ab86303ff51903a0b3052d3c44d1b7ed52
NejahA <achref.nejah@gmail.com>
2023-11-19 11:26:41 +0000

    Counter

---
+++ Python/Week 1/Day 5/Core/Counter/server.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template,session,request,redirect
+
+app= Flask(__name__)
+app.secret_key="secret key"
+@app.route("/")
+def display():
+
+    session["visits"] +=1
+    return render_template("index.html")
+
+@app.route("/addcounter", methods=["POST"])
+def add():
+    print("This is button")
+    
+    session["counter"]+= int(request.form["increments"])
+
+    return redirect("/")
+
+@app.route("/reset", methods=["POST"])
+def reset():
+    session["visits"]=0
+    session["counter"]=0
+    return redirect("/")
+    
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
+
