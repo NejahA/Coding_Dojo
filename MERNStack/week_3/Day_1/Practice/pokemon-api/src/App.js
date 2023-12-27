dfd14bb22c83483a537a51b5990896153c964e55
NejahA <achref.nejah@gmail.com>
2023-12-27 13:50:11 +0000

    Pokemon API All pokemons

--- MERNStack/week_3/Day_1/Practice/pokemon-api/src/App.js
+++ MERNStack/week_3/Day_1/Practice/pokemon-api/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <button
         onClick={() => {
-          fetch("https://pokeapi.co/api/v2/pokemon")
+          fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
             .then((res) => res.json())
             .then((res) => setPokemons(res.results));
         }}
