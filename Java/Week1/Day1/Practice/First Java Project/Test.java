cd418c6a640c62410db53c59f9eaabd955b4e7b5
NejahA <achref.nejah@gmail.com>
2024-01-24 13:41:01 +0000

    My First Java Program

---
+++ Java/Week1/Day1/Practice/First Java Project/Test.java
@@ -0,0 +1,8 @@
+public class Test{
+
+    public static void main(String[] arg){
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I am 28 years old");
+        System.out.println("My name is Hamma");
+    }
+}
